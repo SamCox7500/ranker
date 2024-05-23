a3dde84119a9939e193824cdcea7bdd20d6519b4
Samuel Cox <coxy75@hotmail.co.uk>
2024-05-23 16:05:56 +0100

    Initial commit

---
+++ src/test/java/com/samcox/ranker/RankerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samcox.ranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
