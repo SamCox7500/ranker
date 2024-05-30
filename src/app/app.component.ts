f67eeaccde979d4fa8788dda18e338495429cb6d
Samuel Cox <coxy75@hotmail.co.uk>
2024-05-30 15:42:47 +0100

    Home component and authentication, greeting displayed for authenticated users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http'
+import { HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
-  greeting = {'id': 'XXX', 'content': 'Hello World'};
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
+  logout() {
+    this.http.post('logout', {}).finally(() => {
+      this.app.authenticated = false;
+      this.router.navigateByUrl('login');
+    }).subscribe();
+  }
 }
