645ba8b3ed5137b10bc830b9a3d88fae0b5edcff
Samuel Cox <coxy75@hotmail.co.uk>
2024-07-11 14:37:23 +0100

    RegistrationRequest for post request to register new user.

---
+++ src/main/java/com/samcox/ranker/RegistrationRequest.java
@@ -0,0 +1,19 @@
+package com.samcox.ranker;
+
+public class RegistrationRequest {
+  private String username;
+  private String password;
+
+  public String getUsername() {
+    return username;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
