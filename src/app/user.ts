626029a93fd8279ce3e86190b2ee571ba440afc0
Samuel Cox <coxy75@hotmail.co.uk>
2024-07-29 13:59:36 +0100

    Removed email field from angular user entity.

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,12 +1,10 @@
 export class User {
   id: number;
   username: string;
-  email: string;
 
   constructor() {
     this.id = 0;
     this.username = '';
-    this.email = '';
   }
 
   getId() {
@@ -14,8 +12,5 @@ export class User {
   }
   getUsername() {
     return this.username;
-  }
-  getEmail() {
-    return this.email;
   }
 }
