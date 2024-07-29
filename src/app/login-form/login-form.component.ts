85b8b0db53a0021f11621305889d0b5f8de2cdc6
Samuel Cox <coxy75@hotmail.co.uk>
2024-07-29 16:37:16 +0100

    Login form and login service for frontend authentication.

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { UserCredentials } from '../user-credentials';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [ ReactiveFormsModule ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+export class LoginFormComponent {
+
+ loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  userCredentials: UserCredentials
+
+  constructor(private route: ActivatedRoute, private router: Router, private loginService: LoginService) {
+    this.userCredentials = new UserCredentials();
+  }
+
+  onSubmit() {
+    this.userCredentials.username = this.loginForm.value.username || '';
+    this.userCredentials.password = this.loginForm.value.password || '';
+
+    this.loginService.login(this.userCredentials).subscribe({
+      next: () => goToHome(),
+      error: err => console.error('Login failed', err)
+    });
+  }
+  goToHome() {
+      this.router.navigate(['/home']);
+   }
+}
---
+++ src/app/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserCredentials } from './user-credentials';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private loginUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.loginUrl = 'http://localhost:8080/login'
+  }
+
+  public login(userCredentials: UserCredentials): Observable<any> {
+    return this.http.post(this.loginUrl, userCredentials, { withCredentials: true});
+  }
+
+  public logout(): Observable<any> {
+    return this.http.post('http://localhost:8080/logout', {}, { withCredentials: true});
+  }
+}
