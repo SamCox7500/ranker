4cb4a6f2d5993d672a67b52d81b7a2d02e4e3465
Samuel Cox <coxy75@hotmail.co.uk>
2024-08-13 16:16:25 +0100

    Abstract ranking class. Ranking belongs to one user.

---
+++ src/main/java/com/samcox/ranker/Ranking.java
@@ -0,0 +1,62 @@
+package com.samcox.ranker;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public abstract class Ranking {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  @NotBlank(message = "Ranking must have a user assigned to it")
+  private User user;
+
+  @NotBlank(message = "Ranking must have a title")
+  @Size(min = 1, max = 30, message = "Ranking title must be between 1 and 30 chars")
+  private String title;
+
+  @NotBlank(message = "Ranking must have a description")
+  @Size(min = 1, max = 150, message = "Ranking description must be between 1 and 150 chars")
+  private String description;
+
+  private boolean isPublic;
+
+  //todo type?
+
+  public long getId() {
+    return id;
+  }
+
+  public User getUser() {
+    return user;
+  }
+  public void setUser(User user) {
+    this.user = user;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public boolean getIsPublic() {
+    return isPublic;
+  }
+  public void setPublic() {
+    isPublic = true;
+  }
+  public void setPrivate() {
+    isPublic = false;
+  }
+}
