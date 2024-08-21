3b116b8675fc15e6095a5e1e0fc0559318e49683
Samuel Cox <coxy75@hotmail.co.uk>
2024-08-21 13:39:26 +0100

    Changed type long to Long for consistency

--- src/main/java/com/samcox/ranker/ranking/NumberedRankingController.java
+++ src/main/java/com/samcox/ranker/ranking/NumberedRankingController.java
@@ -15,31 +15,31 @@ public class NumberedRankingController {
     this.numberedRankingService = numberedRankingService;
   }
   @GetMapping("/users/{userId}/numberedranrankings")
-  public List<NumberedRankingDTO> getAllRankings(@PathVariable("userId") long userId) throws AccessDeniedException {
+  public List<NumberedRankingDTO> getAllRankings(@PathVariable("userId") Long userId) throws AccessDeniedException {
     return NumberedRankingDTOMapper.toNumberedRankingDTOs(numberedRankingService
       .getAllNumberedRankingsByUser(userId));
   }
   @GetMapping("/users/{userId}/numberedrankings/{rankingId}")
-  public NumberedRankingDTO getRanking(@PathVariable("rankingId") long rankingId, @PathVariable("userId") long userId) throws AccessDeniedException {
+  public NumberedRankingDTO getRanking(@PathVariable("rankingId") Long rankingId, @PathVariable("userId") Long userId) throws AccessDeniedException {
     return NumberedRankingDTOMapper.toNumberedRankingDTO(numberedRankingService.getNumberedRankingByUserAndId(rankingId, userId));
   }
   //todo should service method have id seperate as well.
   @PostMapping("/users/{userId}/numberedrankings")
-  public void createRanking(@PathVariable("userId") long userId, @Valid NumberedRankingDTO rankingDTO) throws AccessDeniedException {
+  public void createRanking(@PathVariable("userId") Long userId, @Valid NumberedRankingDTO rankingDTO) throws AccessDeniedException {
     if (userId != rankingDTO.getUserDTO().getId()) {
       throw new RuntimeException("UserId Mismatch");
     }
     numberedRankingService.createNumberedRanking(rankingDTO);
   }
   @PutMapping("/users/{userId}/numberedrankings/{rankingId}")
-  public void updateRanking(@PathVariable("userId") long userId, @Valid NumberedRankingDTO rankingDTO) throws AccessDeniedException {
+  public void updateRanking(@PathVariable("userId") Long userId, @Valid NumberedRankingDTO rankingDTO) throws AccessDeniedException {
     if (userId != rankingDTO.getUserDTO().getId()) {
       throw new RuntimeException("UserId Mismatch");
     }
     numberedRankingService.updateNumberedRanking(rankingDTO);
   }
   @DeleteMapping("/users/{userId}/numberedrankings/{rankingId}")
-  public void deleteRanking(@PathVariable("rankingId") long rankingId, @PathVariable("userId") long userId) throws AccessDeniedException {
+  public void deleteRanking(@PathVariable("rankingId") Long rankingId, @PathVariable("userId") Long userId) throws AccessDeniedException {
     numberedRankingService.deleteNumberedRankingByIdAndUser(rankingId, userId);
   }
 }
