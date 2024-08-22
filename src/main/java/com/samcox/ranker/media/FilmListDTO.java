013a3b7d8cdd364a9a4e9f5feacc5675754577ff
Samuel Cox <coxy75@hotmail.co.uk>
2024-08-22 14:14:59 +0100

    Validation constraints for FilmListDTO.

--- src/main/java/com/samcox/ranker/media/FilmListDTO.java
+++ src/main/java/com/samcox/ranker/media/FilmListDTO.java
@@ -1,10 +1,13 @@
 package com.samcox.ranker.media;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public class FilmListDTO {
 
   private Long id;
+  @NotNull
   private List<FilmListEntryDTO> filmListEntryDTOS;
 
 
