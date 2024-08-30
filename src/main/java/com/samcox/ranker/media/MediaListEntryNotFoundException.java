1b754a0e0634f82c6ad6802079e6fb35fd632a54
Samuel Cox <coxy75@hotmail.co.uk>
2024-08-30 15:43:34 +0100

    Service method for adding, removing and moving MediaListEntries in a MediaList implemented.

---
+++ src/main/java/com/samcox/ranker/media/MediaListEntryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.samcox.ranker.media;
+
+public class MediaListEntryNotFoundException extends RuntimeException{
+  public MediaListEntryNotFoundException(String message) {
+    super(message);
+  }
+}
--- src/main/java/com/samcox/ranker/media/MediaListService.java
+++ src/main/java/com/samcox/ranker/media/MediaListService.java
@@ -45,6 +45,27 @@ public class MediaListService {
     mediaListRepository.save(mediaList);
   }
   //todo update
+  public void moveEntryInList(Long mediaListId, int oldPosition, int newPosition) {
+    MediaList mediaList = mediaListRepository.findById(mediaListId)
+      .orElseThrow(() -> new MediaListNotFoundException("MediaList not found with id: " + mediaListId));
+    mediaList.moveEntry(oldPosition, newPosition);
+    mediaListRepository.save(mediaList);
+  }
+  public void removeEntryInList(Long mediaListId, Long entryId) {
+    MediaList mediaList = mediaListRepository.findById(mediaListId)
+      .orElseThrow(() -> new MediaListNotFoundException("Media list not found with id: " + mediaListId));
+
+    MediaListEntry entry = mediaList.getEntries().stream()
+      .filter(e -> e.getId().equals(entryId))
+      .findFirst()
+      .orElseThrow(() -> new MediaListEntryNotFoundException("MediaListEntry not found with id: " + entryId));
+  }
+  public void addEntryToList(Long mediaListId, MediaListEntry newEntry) {
+    MediaList mediaList = mediaListRepository.findById(mediaListId)
+      .orElseThrow(() -> new MediaListNotFoundException("MediaList not found with id: " + mediaListId));
+    mediaList.addEntry(newEntry);
+    mediaListRepository.save(mediaList);
+  }
   public void deleteMediaListByNumberedRankingAndUser(Long numberedRankingId, Long userId) throws AccessDeniedException {
     NumberedRanking numberedRanking = numberedRankingService.getNumberedRankingByUserAndId(numberedRankingId, userId);
     MediaList mediaList = mediaListRepository.findByNumberedRanking(numberedRanking)
