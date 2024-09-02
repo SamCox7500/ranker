68fa68e3851deba018a144fa08c445ab240b7639
Samuel Cox <coxy75@hotmail.co.uk>
2024-09-02 12:25:05 +0100

    Added more fields for data on media retrieved from TMDB API in MediaListEntryDTO.

--- src/main/java/com/samcox/ranker/media/MediaListEntryDTO.java
+++ src/main/java/com/samcox/ranker/media/MediaListEntryDTO.java
@@ -5,35 +5,47 @@ import jakarta.validation.constraints.NotNull;
 
 public class MediaListEntryDTO {
   private Long id;
-  @NotNull
-  private int ranking;
-
   private String tmdbId;
+  private int ranking;
+  private String title;
+  private MediaType mediaType;
 
-  public MediaListEntryDTO(Long id, int ranking, String tmdbId) {
-
+  public MediaListEntryDTO(Long id, String tmdbId, int ranking, String title, MediaType mediaType) {
+    this.id = id;
+    this.tmdbId = tmdbId;
+    this.ranking = ranking;
+    this.title = title;
+    this.mediaType = mediaType;
   }
 
   public Long getId() {
     return id;
   }
-
   public void setId(Long id) {
     this.id = id;
   }
-
-  public int getRanking() {
-    return ranking;
-  }
-
-  public void setRanking(int ranking) {
-    this.ranking = ranking;
-  }
-
   public String getTmdbId() {
     return tmdbId;
   }
   public void setTmdbId(String tmdbId) {
     this.tmdbId = tmdbId;
+  }
+  public int getRanking() {
+    return ranking;
+  }
+  public void setRanking(int ranking) {
+    this.ranking = ranking;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public MediaType getMediaType() {
+    return mediaType;
+  }
+  public void setMediaType(MediaType mediaType) {
+    this.mediaType = mediaType;
   }
 }
