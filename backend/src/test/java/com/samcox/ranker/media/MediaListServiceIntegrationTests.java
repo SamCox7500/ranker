913227f93387ce8303147729c75abd42c0fc7fd7
Sam Cox <coxy75@hotmail.co.uk>
2025-08-22 15:28:51 +0100

    Refactored MediaListServiceIntegrationTests to account for refactoring of MediaListService.

--- backend/src/test/java/com/samcox/ranker/media/MediaListServiceIntegrationTests.java
+++ backend/src/test/java/com/samcox/ranker/media/MediaListServiceIntegrationTests.java
@@ -104,6 +104,7 @@ public class MediaListServiceIntegrationTests {
 
     numberedRankingRepository.save(testNumberedRanking);
   }
+  /*
   @Test
   @WithMockUser("testuser")
   public void testGetMediaListByNumberedRankingAndUser_Success() throws AccessDeniedException {
@@ -121,227 +122,256 @@ public class MediaListServiceIntegrationTests {
     assertEquals(entries.get(1), testMediaList.getEntries().get(1));
     assertEquals(entries.get(2), testMediaList.getEntries().get(2));
 
-    /*
-    for(MediaListEntry entry: entries) {
-      System.out.println(entry);
-    }
-     */
-
   }
+  */
+  /*
   @Test
   @WithMockUser("testuser1")
   public void testGetMediaListByNumberedRankingAndUser_NotAuthorized() throws AccessDeniedException {
     assertThrows(AccessDeniedException.class,
       () -> mediaListService.getMediaListByNumberedRankingAndUser(testNumberedRanking.getId(), testUser.getId()));
   }
+   */
+  /*
   @Test
   @WithMockUser("testuser")
   public void testGetMediaListByNumberedRankingAndUser_InvalidRanking() throws AccessDeniedException {
     assertThrows(RankingNotFoundException.class,
       () -> mediaListService.getMediaListByNumberedRankingAndUser(999L, testUser.getId()));
   }
+   */
+  /*
   @Test
   @WithMockUser("testuser")
   public void testGetMediaListByNumberedRankingAndUser_InvalidUser() throws AccessDeniedException {
     assertThrows(AccessDeniedException.class,
       () -> mediaListService.getMediaListByNumberedRankingAndUser(testNumberedRanking.getId(), 999L));
   }
+   */
   @Test
   @WithMockUser("testuser")
   public void testMoveEntry_Success() throws AccessDeniedException {
+    Long userId = testUser.getId();
     //Number 1 in list should become number 2, number 2 should become number 1
-    mediaListService.moveEntryInList(testMediaList.getId(), 1, 2);
+    mediaListService.moveEntryInList(userId, testMediaList.getId(), 1, 2);
     MediaList mediaList = mediaListRepository.findById(testMediaList.getId()).orElseThrow();
 
-    assertEquals(mediaList.getEntries().get(0).getRanking(), 1);
-    assertEquals(mediaList.getEntries().get(0).getTmdbId(), 115L);
-    assertEquals(mediaList.getEntries().get(0).getMediaList().getId(), testMediaList.getId());
+    assertEquals(1, mediaList.getEntries().get(0).getRanking());
+    assertEquals(115L, mediaList.getEntries().get(0).getTmdbId());
+    assertEquals(testMediaList.getId(), mediaList.getEntries().get(0).getMediaList().getId());
 
-    assertEquals(mediaList.getEntries().get(1).getRanking(), 2);
-    assertEquals(mediaList.getEntries().get(1).getTmdbId(), 7345L);
-    assertEquals(mediaList.getEntries().get(1).getMediaList().getId(), testMediaList.getId());
+    assertEquals(2, mediaList.getEntries().get(1).getRanking());
+    assertEquals(7345L, mediaList.getEntries().get(1).getTmdbId());
+    assertEquals(testMediaList.getId(), mediaList.getEntries().get(1).getMediaList().getId());
 
-    assertEquals(mediaList.getEntries().get(2).getRanking(), 3);
-    assertEquals(mediaList.getEntries().get(2).getTmdbId(), 4638L);
-    assertEquals(mediaList.getEntries().get(2).getMediaList().getId(), testMediaList.getId());
+    assertEquals(3, mediaList.getEntries().get(2).getRanking());
+    assertEquals(4638L, mediaList.getEntries().get(2).getTmdbId());
+    assertEquals(testMediaList.getId(), mediaList.getEntries().get(2).getMediaList().getId());
 
     MediaListEntry mediaListEntry = mediaListEntryRepository.findByMediaListAndId(mediaList, mediaList.getEntries().get(0).getId()).orElseThrow();
-    assertEquals(mediaListEntry.getRanking(), 1);
-    assertEquals(mediaListEntry.getTmdbId(), 115L);
+    assertEquals(1, mediaListEntry.getRanking());
+    assertEquals(115L, mediaListEntry.getTmdbId());
     MediaListEntry mediaListEntry1 = mediaListEntryRepository.findByMediaListAndId(mediaList, mediaList.getEntries().get(1).getId()).orElseThrow();
-    assertEquals(mediaListEntry1.getRanking(), 2);
-    assertEquals(mediaListEntry1.getTmdbId(), 7345L);
+    assertEquals(2, mediaListEntry1.getRanking());
+    assertEquals(7345L, mediaListEntry1.getTmdbId());
 
   }
   @Test
   @WithMockUser("testuser")
   public void testMoveEntryMultiple_Success() throws AccessDeniedException {
+    Long userId = testUser.getId();
     System.out.println("Initial media list state: " + testMediaList.getEntries());
-    mediaListService.moveEntryInList(testMediaList.getId(), 1, 2);
+    mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 1, 2);
     System.out.println("Media list after swapping 1 and 2: " + testMediaList.getEntries());
-    mediaListService.moveEntryInList(testMediaList.getId(), 1, 2);
+    mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 1, 2);
     System.out.println("Media list after swapping 1 and 2 again: " + testMediaList.getEntries());
 
     //Should be original order
     MediaList mediaList = mediaListRepository.findById(testMediaList.getId()).orElseThrow();
 
-    assertEquals(mediaList.getEntries().get(0).getRanking(), 1);
-    assertEquals(mediaList.getEntries().get(0).getTmdbId(), 7345L);
-    assertEquals(mediaList.getEntries().get(0).getMediaList().getId(), testMediaList.getId());
+    assertEquals(1, mediaList.getEntries().get(0).getRanking());
+    assertEquals(7345L, mediaList.getEntries().get(0).getTmdbId());
+    assertEquals(testMediaList.getId(), mediaList.getEntries().get(0).getMediaList().getId());
 
-    assertEquals(mediaList.getEntries().get(1).getRanking(), 2);
-    assertEquals(mediaList.getEntries().get(1).getTmdbId(), 115L);
-    assertEquals(mediaList.getEntries().get(1).getMediaList().getId(), testMediaList.getId());
+    assertEquals(2, mediaList.getEntries().get(1).getRanking());
+    assertEquals(115L, mediaList.getEntries().get(1).getTmdbId());
+    assertEquals(testMediaList.getId(), mediaList.getEntries().get(1).getMediaList().getId());
 
-    assertEquals(mediaList.getEntries().get(2).getRanking(), 3);
-    assertEquals(mediaList.getEntries().get(2).getTmdbId(), 4638L);
-    assertEquals(mediaList.getEntries().get(2).getMediaList().getId(), testMediaList.getId());
+    assertEquals(3, mediaList.getEntries().get(2).getRanking());
+    assertEquals(4638L, mediaList.getEntries().get(2).getTmdbId());
+    assertEquals(testMediaList.getId(), mediaList.getEntries().get(2).getMediaList().getId());
 
   }
   @Test
   @WithMockUser("testuser1")
   public void testMoveEntry_NotAuthorized() throws AccessDeniedException {
     assertThrows(AccessDeniedException.class,
-      () ->  mediaListService.moveEntryInList(testMediaList.getId(), 1, 2));
+      () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 1, 2));
   }
   @Test
   @WithMockUser("testuser")
   public void testMoveEntry_InvalidMediaListId() throws AccessDeniedException {
     assertThrows(MediaListNotFoundException.class,
-      () ->  mediaListService.moveEntryInList(999L, 1, 2));
+      () ->  mediaListService.moveEntryInList(testUser.getId(),999L, 1, 2));
   }
   @Test
   @WithMockUser("testuser")
   public void testMoveEntry_InvalidOldPosition() throws AccessDeniedException {
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testMediaList.getId(), 4, 1));
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testMediaList.getId(), 0, 1));
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testMediaList.getId(), -1, 2));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 4, 1));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 0, 1));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), -1, 2));
   }
   @Test
   @WithMockUser("testuser")
   public void testMoveEntry_InvalidNewPosition() throws AccessDeniedException {
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testMediaList.getId(), 3, 4));
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testMediaList.getId(), 3, 0));
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testMediaList.getId(), 2, -1));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 3, 4));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 3, 0));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.moveEntryInList(testUser.getId(), testMediaList.getId(), 2, -1));
   }
   @Test
   @WithMockUser("testuser")
   public void testRemoveEntry_Success() throws AccessDeniedException {
-    mediaListService.removeEntryInList(testMediaList.getId(), testMediaListEntry.getId());
+    Long userId = testUser.getId();
+    mediaListService.removeEntryInList(testUser.getId(), testMediaList.getId(), testMediaListEntry.getId());
 
     List<MediaListEntry> entries = mediaListRepository.findById(testMediaList.getId()).get().getEntries();
-    assertEquals(entries.size(), 2);
+    assertEquals(2, entries.size());
 
-    assertEquals(entries.get(0).getTmdbId(), 115L);
-    assertEquals(entries.get(0).getRanking(), 1);
+    assertEquals(115L, entries.get(0).getTmdbId());
+    assertEquals(1, entries.get(0).getRanking());
 
-    assertEquals(entries.get(1).getTmdbId(), 4638L);
-    assertEquals(entries.get(1).getRanking(), 2);
+    assertEquals(4638L, entries.get(1).getTmdbId());
+    assertEquals(2, entries.get(1).getRanking());
   }
   @Test
   @WithMockUser("testuser1")
   public void testRemoveEntry_NotAuthorized() throws AccessDeniedException {
-    assertThrows(AccessDeniedException.class, () ->  mediaListService.removeEntryInList(testMediaList.getId(), testMediaListEntry.getId()));
+    Long userId = testUser.getId();
+    assertThrows(AccessDeniedException.class, () ->  mediaListService.removeEntryInList(testUser.getId(), testMediaList.getId(), testMediaListEntry.getId()));
   }
   @Test
   @WithMockUser("testuser")
   public void testRemoveEntry_InvalidEntry() throws AccessDeniedException {
-    assertThrows(MediaListEntryNotFoundException.class, () ->  mediaListService.removeEntryInList(testMediaList.getId(), 999L));
+    assertThrows(MediaListEntryNotFoundException.class, () ->  mediaListService.removeEntryInList(testUser.getId(), testMediaList.getId(), 999L));
   }
   @Test
   @WithMockUser("testuser")
   public void testRemoveEntries_Success() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     List<Long> entriesToDelete = new ArrayList<>();
     entriesToDelete.add(testMediaListEntry.getId());
     entriesToDelete.add(testMediaListEntry2.getId());
 
-    mediaListService.removeEntriesInList(testMediaList.getId(), entriesToDelete);
+    mediaListService.removeEntriesInList(userId, testMediaList.getId(), entriesToDelete);
 
     List<MediaListEntry> entries = mediaListRepository.findById(testMediaList.getId()).get().getEntries();
-    assertEquals(entries.size(), 1);
-    assertEquals(entries.get(0).getTmdbId(), 115L);
-    assertEquals(entries.get(0).getRanking(),1);
+    assertEquals(1, entries.size());
+    assertEquals(115L, entries.get(0).getTmdbId());
+    assertEquals(1, entries.get(0).getRanking());
   }
   @Test
   @WithMockUser("testuser1")
   public void testRemoveEntries_NotAuthorized() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     List<Long> entriesToDelete = new ArrayList<>();
     entriesToDelete.add(testMediaListEntry.getId());
     entriesToDelete.add(testMediaListEntry2.getId());
 
-    assertThrows(AccessDeniedException.class, () ->  mediaListService.removeEntriesInList(testMediaList.getId(), entriesToDelete));
+    assertThrows(AccessDeniedException.class, () ->  mediaListService.removeEntriesInList(userId, testMediaList.getId(), entriesToDelete));
   }
   @Test
   @WithMockUser("testuser")
   public void testRemoveEntries_InvalidEntries() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     List<Long> entriesToDelete = new ArrayList<>();
     entriesToDelete.add(testMediaListEntry.getId());
     entriesToDelete.add(999L);
 
-    assertThrows(MediaListEntryNotFoundException.class, () ->  mediaListService.removeEntriesInList(testMediaList.getId(), entriesToDelete));
+    assertThrows(MediaListEntryNotFoundException.class, () ->  mediaListService.removeEntriesInList(userId, testMediaList.getId(), entriesToDelete));
   }
   @Test
   @WithMockUser("testuser")
   public void testRemoveEntries_NoEntriesSpecified() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     List<Long> entriesToDelete = new ArrayList<>();
-    assertThrows(MediaListEntryNotFoundException.class, () ->  mediaListService.removeEntriesInList(testMediaList.getId(), entriesToDelete));
+    assertThrows(MediaListEntryNotFoundException.class, () ->  mediaListService.removeEntriesInList(userId, testMediaList.getId(), entriesToDelete));
   }
   @Test
   @WithMockUser("testuser")
   public void testAddEntry_Success() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     EntryAddRequest entryAddRequest = new EntryAddRequest();
     entryAddRequest.setTmdbId(299534L);
     entryAddRequest.setRanking(2);
 
-    mediaListService.addEntryToList(entryAddRequest, testMediaList.getId());
+    mediaListService.addEntryToList(userId, entryAddRequest, testMediaList.getId());
 
     List<MediaListEntry> entries = mediaListRepository.findById(testMediaList.getId()).get().getEntries();
     assertEquals(4, entries.size());
 
 
-    assertEquals(entries.get(0).getRanking(), 1);
-    assertEquals(entries.get(0).getTmdbId(), 7345L);
-    assertEquals(entries.get(0).getMediaList().getId(), testMediaList.getId());
+    assertEquals(1, entries.get(0).getRanking());
+    assertEquals(7345L, entries.get(0).getTmdbId());
+    assertEquals(testMediaList.getId(), entries.get(0).getMediaList().getId());
 
-    assertEquals(entries.get(1).getRanking(), 2);
-    assertEquals(entries.get(1).getTmdbId(), 299534L);
-    assertEquals(entries.get(1).getMediaList().getId(), testMediaList.getId());
+    assertEquals(2, entries.get(1).getRanking());
+    assertEquals(299534L, entries.get(1).getTmdbId());
+    assertEquals(testMediaList.getId(), entries.get(1).getMediaList().getId());
 
-    assertEquals(entries.get(2).getRanking(), 3);
-    assertEquals(entries.get(2).getTmdbId(), 115L);
-    assertEquals(entries.get(2).getMediaList().getId(), testMediaList.getId());
+    assertEquals(3, entries.get(2).getRanking());
+    assertEquals(115L, entries.get(2).getTmdbId());
+    assertEquals(testMediaList.getId(), entries.get(2).getMediaList().getId());
 
-    assertEquals(entries.get(3).getRanking(), 4);
-    assertEquals(entries.get(3).getTmdbId(), 4638L);
-    assertEquals(entries.get(3).getMediaList().getId(), testMediaList.getId());
+    assertEquals(4, entries.get(3).getRanking());
+    assertEquals(4638L, entries.get(3).getTmdbId());
+    assertEquals(testMediaList.getId(), entries.get(3).getMediaList().getId());
 
   }
   @Test
   @WithMockUser("testuser1")
   public void testAddEntry_NotAuthorized() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     EntryAddRequest entryAddRequest = new EntryAddRequest();
     entryAddRequest.setTmdbId(299534L);
     entryAddRequest.setRanking(2);
 
-    assertThrows(AccessDeniedException.class, () ->  mediaListService.addEntryToList(entryAddRequest, testMediaList.getId()));
+    assertThrows(AccessDeniedException.class, () ->  mediaListService.addEntryToList(userId, entryAddRequest, testMediaList.getId()));
   }
   @Test
   @WithMockUser("testuser")
   public void testAddEntry_InvalidMediaListId() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     EntryAddRequest entryAddRequest = new EntryAddRequest();
     entryAddRequest.setTmdbId(299534L);
     entryAddRequest.setRanking(2);
 
-    assertThrows(MediaListNotFoundException.class, () ->  mediaListService.addEntryToList(entryAddRequest, 999L));
+    assertThrows(MediaListNotFoundException.class, () ->  mediaListService.addEntryToList(userId, entryAddRequest, 999L));
   }
   @Test
   @WithMockUser("testuser")
   public void testAddEntry_NullTmdbId() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     EntryAddRequest entryAddRequest = new EntryAddRequest();
     //entryAddRequest.setTmdbId(299534L);
     entryAddRequest.setRanking(2);
 
-    assertThrows(ConstraintViolationException.class, () ->  mediaListService.addEntryToList(entryAddRequest, testMediaList.getId()));
+    assertThrows(ConstraintViolationException.class, () ->  mediaListService.addEntryToList(userId, entryAddRequest, testMediaList.getId()));
   }
   @Test
   @WithMockUser("testuser")
@@ -351,29 +381,25 @@ public class MediaListServiceIntegrationTests {
   @Test
   @WithMockUser("testuser")
   public void testAddEntry_InvalidRanking() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     EntryAddRequest entryAddRequest = new EntryAddRequest();
     entryAddRequest.setTmdbId(299534L);
     entryAddRequest.setRanking(5);
 
-    assertThrows(IllegalArgumentException.class, () ->  mediaListService.addEntryToList(entryAddRequest, testMediaList.getId()));
+    assertThrows(IllegalArgumentException.class, () ->  mediaListService.addEntryToList(userId, entryAddRequest, testMediaList.getId()));
   }
   @Test
   @WithMockUser("testuser")
   public void testAddEntry_DuplicateMediaId() throws AccessDeniedException {
+
+    Long userId = testUser.getId();
+
     EntryAddRequest entryAddRequest = new EntryAddRequest();
     entryAddRequest.setTmdbId(115L);
     entryAddRequest.setRanking(4);
 
-    assertThrows(DuplicateMediaEntryException.class, () ->  mediaListService.addEntryToList(entryAddRequest, testMediaList.getId()));
-  }
-  @Test
-  @WithMockUser("testuser")
-  public void checkOwnership_Success() throws AccessDeniedException {
-    mediaListService.checkOwnership(testMediaList.getId());
-  }
-  @Test
-  @WithMockUser("testuser1")
-  public void checkOwnership_InvalidOwner() throws AccessDeniedException {
-    assertThrows(AccessDeniedException.class, () -> mediaListService.checkOwnership(testMediaList.getId()));
+    assertThrows(DuplicateMediaEntryException.class, () ->  mediaListService.addEntryToList(userId, entryAddRequest, testMediaList.getId()));
   }
 }
